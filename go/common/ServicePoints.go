ec5c42efbeb2e2873f27d78ebc68bf6bb2d4509f
Sharon Aicler <saichler@gmail.com>
2025-03-20 07:40:31 -0700

    Topic to multicast

--- go/common/ServicePoints.go
+++ go/common/ServicePoints.go
@@ -21,6 +21,7 @@ type IServicePointHandler interface {
 	GetCopy(proto.Message, IResources) (proto.Message, error)
 	Get(proto.Message, IResources) (proto.Message, error)
 	Failed(proto.Message, IResources, *types.Message) (proto.Message, error)
+	CheckProto(message proto.Message) bool
 	EndPoint() string
 	Multicast() string
 	Transactional() bool
