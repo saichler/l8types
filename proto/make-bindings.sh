632b172a6847589c919c4cf113cd56ddf682c582
Sharon Aicler <saichler@gmail.com>
2025-03-20 05:39:03 -0700

    readd destination to messages

--- proto/make-bindings.sh
+++ proto/make-bindings.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Use the protoc image to run protoc.sh and generate the bindings.
-docker run -e PROTO=message.proto --mount type=bind,source="$PWD",target=/home/proto/ -it saichler/protoc:latest
-docker run -e PROTO=notification.proto --mount type=bind,source="$PWD",target=/home/proto/ -it saichler/protoc:latest
-docker run -e PROTO=reflect.proto --mount type=bind,source="$PWD",target=/home/proto/ -it saichler/protoc:latest
-docker run -e PROTO=tests.proto --mount type=bind,source="$PWD",target=/home/proto/ -it saichler/protoc:latest
+docker run --user "$(id -u):$(id -g)" -e PROTO=message.proto --mount type=bind,source="$PWD",target=/home/proto/ -it saichler/protoc:latest
+docker run --user "$(id -u):$(id -g)" -e PROTO=notification.proto --mount type=bind,source="$PWD",target=/home/proto/ -it saichler/protoc:latest
+docker run --user "$(id -u):$(id -g)" -e PROTO=reflect.proto --mount type=bind,source="$PWD",target=/home/proto/ -it saichler/protoc:latest
+docker run --user "$(id -u):$(id -g)" -e PROTO=tests.proto --mount type=bind,source="$PWD",target=/home/proto/ -it saichler/protoc:latest
 
 # Now move the generated bindings to the models directory and clean up
 mkdir -p ../go/types
