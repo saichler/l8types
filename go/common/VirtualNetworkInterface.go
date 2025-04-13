ae316277b30d342e74b3eb09f719cab4cb79ff4d
Sharon Aicler <saichler@gmail.com>
2025-04-13 08:26:40 -0700

    Refactor service points

--- go/common/VirtualNetworkInterface.go
+++ go/common/VirtualNetworkInterface.go
@@ -54,6 +54,7 @@ type IVirtualNetworkInterface interface {
 	API(string, uint16) API
 	Resources() IResources
 	NotifyServiceAdded() error
+	NotifyServiceRemoved(string, uint16) error
 	PropertyChangeNotification(*types.NotificationSet)
 }
 
