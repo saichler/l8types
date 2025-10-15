15c70477a7f4094dad5f0609fe17f02e8f6ad574
Sharon Aicler <saichler@gmail.com>
2025-10-15 06:42:01 -0700

    Add Health List

--- go/ifs/ServiceConfig.go
+++ go/ifs/ServiceConfig.go
@@ -13,4 +13,18 @@ type ServiceConfig struct {
 	Replication       bool
 	ReplicationCount  int
 	WebServiceDef     IWebService
+	Callback          IServiceCallback
+}
+
+type IServiceCallback interface {
+	BeforePost(IElements, IVNic) IElements
+	AfterPost(IElements, IVNic) IElements
+	BeforePut(IElements, IVNic) IElements
+	AfterPut(IElements, IVNic) IElements
+	BeforePatch(IElements, IVNic) IElements
+	AfterPatch(IElements, IVNic) IElements
+	BeforeDelete(IElements, IVNic) IElements
+	AfterDelete(IElements, IVNic) IElements
+	BeforeGet(IElements, IVNic) IElements
+	AfterGet(IElements, IVNic) IElements
 }
